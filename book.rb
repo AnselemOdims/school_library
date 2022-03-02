99da7c56e048707087ed9205e13fb71777e6066c
Anselem <anselemodims@gmail.com>
2022-03-02 11:57:56 +0100

    Feat: modify the constructor of Rental so Book and Person are set in it

--- book.rb
+++ book.rb
@@ -1,6 +1,5 @@
 class Book
-  attr_accessor :title
-  attr_accessor :author
+  attr_accessor :title, :author
   attr_reader :rentals
 
   def initialize(title, author)
@@ -12,4 +11,4 @@ class Book
   def add_rental(rental)
     @rentals << rental
   end
-end 
+end
--- classroom.rb
+++ classroom.rb
@@ -13,12 +13,3 @@ class Classroom
     @students << student
   end
 end
-
-chem = Classroom.new("chemistry")
-phy = Classroom.new("physics")
-john = Student.new(chem, 18, "John")
-jane = Student.new(chem, 16, "Jane")
-john.classroom = phy
-chem.add_student(john)
-chem.add_student(jane)
-p chem.students.map {|student| student.classroom.label}
