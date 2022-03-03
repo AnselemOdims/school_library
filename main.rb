c9f7579d5fc3956775e8d4288698caed48105a9c
Anselem <anselemodims@gmail.com>
2022-03-03 15:30:13 +0100

    Feat: list all people

--- main.rb
+++ main.rb
@@ -1,5 +1,6 @@
 require_relative './book'
 
+# list all books
 def list_books
   books = Book.all
   if books.empty?
@@ -7,6 +8,18 @@ def list_books
   else
     books.each do |book|
       puts "Title: \"#{book.title}\", Author: #{book.author}"
+    end
+  end
+end
+
+# list all people
+def list_people
+  people = Person.all
+  if people.empty?
+    puts "No person available. Please add  teacher or student"
+  else
+    people.each do |person|
+      puts "[#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
     end
   end
 end
