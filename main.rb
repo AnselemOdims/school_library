7f5ed47c5a8e4a4b665a6a0a048255b9cd2ab1aa
Anselem <anselemodims@gmail.com>
2022-03-03 16:34:30 +0100

    Feat: add functions to display welcome message and list of options

--- main.rb
+++ main.rb
@@ -55,3 +55,20 @@ end
 def list_rental(id)
   Person.all.find_all{|person| person.id == id.to_i}
 end
+
+# display welcome message
+def welcome 
+  puts "Welcome to School Library App!"
+end
+
+# display a list of options
+def choose 
+  puts "Please choose an option by entering a number: "
+  puts "1 - List all books"
+  puts "2 - List all people"
+  puts "3 - Create a person"
+  puts "4 - Create a book"
+  puts "5 - Create a rental"
+  puts "6 - List all of rentals for a given person id"
+  puts "7 - Exit"
+end 
