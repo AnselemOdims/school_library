30e18f6a48f8579120eaa25ebc1913c6d07a1b11
Anselem <anselemodims@gmail.com>
2022-03-08 19:08:38 +0100

    Feat: list books and people from their respective json files

--- list_items.rb
+++ list_items.rb
@@ -7,7 +7,7 @@ module List
       puts 'No books available. Please add books'
     else
       books.each do |book|
-        puts "Title: \"#{book.title}\", Author: #{book.author}"
+        puts "Title: \"#{book['title']}\", Author: #{book['author']}"
       end
     end
   end
@@ -18,7 +18,7 @@ module List
       puts 'No person available. Please add  teacher or student'
     else
       people.each do |person|
-        puts "[#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
+        puts "[#{person['type']}] Name: #{person['name']}, ID: #{person['id']}, Age: #{person['age']}"
       end
     end
   end
